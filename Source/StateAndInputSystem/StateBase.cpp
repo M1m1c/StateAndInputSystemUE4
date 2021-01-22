b872148c7ac2ba5c0cf8219c1c6b36cccb10ec73
LAPTOP-QCSFF0C1\L B O <ludvigbaumolss@hotmail.se>
2021-01-22 16:56:55 +0100

    removed unused includes

--- Source/StateAndInputSystem/StateBase.cpp
+++ Source/StateAndInputSystem/StateBase.cpp
@@ -1,8 +1,6 @@
 // © 2018, Ludvig Baummann Olsson. All rights reserved.
 
 #include "StateBase.h"
-#include "SampleCharacter.h"
-#include "SamplePlayerController.h"
 #include "StateParametersBase.h"
 
 
